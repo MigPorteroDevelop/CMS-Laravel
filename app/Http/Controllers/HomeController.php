6cf69bf42752d0564c25000f8ef119fbdb7db904
Miguel <migporuiz@gmail.com>
2023-10-10 11:35:18 +0200

    Users&Roles&Permissions&Edit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $posts = Post::all();
-        return view('home', ['posts' => $posts]);
+        $categories = Category::all();
+        return view('home', ['posts' => $posts, 'categories' => $categories]);
+    }
+
+    public function view($slug)
+    {
+        $post = Post::where('slug', $slug)->first();
+        $categories = Category::all();
+        return view('view', ['post' => $post, 'categories' => $categories]);
     }
 }
--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -75,54 +75,27 @@
     </nav>
 
 
-    <div class="container mx-auto flex flex-wrap py-6">
+    <div class="container mx-auto flex flex-wrap py-6 justify-center">
 
         <!-- Posts Section -->
         <section class="w-full md:w-2/3 flex flex-col items-center px-3">
-            @foreach ($posts as $post)
-                <article class="flex flex-col shadow w-4/5 my-4">
-                    <!-- Article Image -->
-                    <a href="/{{$post->slug}}" class="hover:opacity-75">
-                        <img src="https://localhost:8000/storage/{{ $post->image_url }}">
-                    </a>
-                    <div class="bg-white flex flex-col justify-start p-6">
-                        <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4">{{ $post->category->name }}</a>
-                        <a href="/{{$post->slug}}"  class="text-3xl font-bold hover:text-gray-700 pb-4">{{ $post->title }}</a>
-                        <p href="#" class="text-sm pb-3">
-                            By <a href="#" class="font-semibold hover:text-gray-800">{{ $post->user->name }}</a>, Published on {{ $post->created_at }}
-                        </p>
-                        <a href="#" class="pb-6">{!! \Illuminate\Support\Str::limit($post->body, 80, '...') !!}</a>
-                        <a href="/{{$post->slug}}" class="uppercase text-gray-800 hover:text-black">Continue<i class="fas fa-arrow-right"></i></a>
-                    </div>
-                </article>
-            @endforeach
-            <!-- Pagination -->
-            <div class="flex items-center py-8">
-                <a href="#" class="h-10 w-10 bg-blue-800 hover:bg-blue-600 font-semibold text-white text-sm flex items-center justify-center">1</a>
-                <a href="#" class="h-10 w-10 font-semibold text-gray-800 hover:bg-blue-600 hover:text-white text-sm flex items-center justify-center">2</a>
-                <a href="#" class="h-10 w-10 font-semibold text-gray-800 hover:text-gray-900 text-sm flex items-center justify-center ml-3">Next <i class="fas fa-arrow-right ml-2"></i></a>
-            </div>
-
-        </section>
-
-        <!-- Sidebar Section -->
-        <aside class="w-full md:w-1/3 flex flex-col items-center px-3">
-
-            <div class="w-full bg-white shadow flex flex-col my-4 p-6">
-                <p class="text-xl font-semibold pb-5">About Us</p>
-                <p class="pb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mattis est eu odio sagittis tristique. Vestibulum ut finibus leo. In hac habitasse platea dictumst.</p>
-                <a href="#" class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
-                    Get to know us
-                </a>
-            </div>
-
-
-        </aside>
-
+            <article class="flex flex-col shadow my-4">
+                <!-- Article Image -->
+                <a href="/{{$post->slug}}" class="hover:opacity-75">
+                    <img src="https://localhost:8000/storage/{{ $post->image_url }}">
+                   </a>
+                <div class="bg-white flex flex-col justify-start p-6">
+                    <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4">{{ $post->category->name }}</a>
+                    <a href="/{{$post->slug}}"  class="text-3xl font-bold hover:text-gray-700 pb-4">{{ $post->title }}</a>
+                    <p href="#" class="text-sm pb-3">
+                    By <a href="#" class="font-semibold hover:text-gray-800">{{ $post->user->name }}</a>, Published on {{ $post->created_at }}                        </p>
+                       <a href="#" class="pb-6">{!! ($post->body) !!}</a>
+                </div>
+            </article>
+        </section>    
     </div>
 
-    <footer class="w-full border-t bg-white pb-12">
-        
+    <footer class="w-full border-t bg-white pb-12">       
         <div class="w-full container mx-auto flex flex-col items-center">
             <div class="flex flex-col md:flex-row text-center md:text-left md:justify-between py-6">
                 <a href="#" class="uppercase px-3">About Us</a>
@@ -133,8 +106,5 @@
             <div class="uppercase pb-6">&copy; myblog.com</div>
         </div>
     </footer>
-
-    
-
 </body>
 </html>
